f29645445856f33fed6a774e35afca1cb4ddc122
Bangaly KAKE <bangaly.kake-prestataire@docapost.fr>
2024-06-26 13:52:04 +0200

    Refactoring

--- order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/payment/OrderCancelledPaymentRequestMessagePublisher.java
+++ order-service/order-domain/order-application-service/src/main/java/com/food/ordering/system/order/service/domain/ports/output/message/publisher/payment/OrderCancelledPaymentRequestMessagePublisher.java
@@ -1,7 +1,8 @@
 package com.food.ordering.system.order.service.domain.ports.output.message.publisher.payment;
 
 import com.food.ordering.system.event.DomainEventPublisher;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
 import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
 
-public interface OrderCancelledPaymentRequestMessagePublisher extends DomainEventPublisher<OrderCreatedEvent> {
+public interface OrderCancelledPaymentRequestMessagePublisher extends DomainEventPublisher<OrderCancelledEvent> {
 }
